d52c60b60ca6093ec409626cffcab308656e4d5b
Clarion Heard <clarion.heard@gmail.com>
2020-01-04 20:40:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,14 +1,9 @@
 # Your Code Here
-def negativize(array)
-  new=[]
-  i=0 
-  while i<array.length do
-    array[i] *= -1
-    new << array[i]
-    i+=1 
-  end
-  new
-end
 
+  new=[]
+  i = 0 
+  while i<source_array.length do
+    
 def map(source_array, block)
+  source_array.each {b
   
