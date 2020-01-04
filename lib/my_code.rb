ebacb6ed538dc7e814521c653f535b1a481baf7a
Clarion Heard <clarion.heard@gmail.com>
2020-01-04 20:26:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
 # Your Code Here
+def negativize(array)
+  new=[]
+  i=0 
+  while i<array.length do
+    array[i] *= -1
+    new << array[i]
+    i+=1 
+  end
+  new
+end
+
+def map(source_array, block)
+  
