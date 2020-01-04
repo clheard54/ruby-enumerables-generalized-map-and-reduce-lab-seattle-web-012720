f3c48b8bdff0590fd01d1f9e72fe30d13476b519
Clarion Heard <clarion.heard@gmail.com>
2020-01-04 21:10:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,5 +8,12 @@ def map(source_array)
   new
 end 
 
-def reduce(array)
-  result = 
+def reduce(source_array, starting_point=0)
+  new = [starting_point]
+  i = 0
+  while i < source_array.length do
+    new[i+1] = yield(new[i], source_array[i])
+    i+=1
+  end
+  return new[-1]
+end
